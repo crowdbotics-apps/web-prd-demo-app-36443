ee8f301e789be41d1b44e408e3272abc337d995d
Crowdbotics <team@crowdbotics.com>
2022-07-27 17:42:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/web_prd_demo_app_36443/MainActivity.java
@@ -0,0 +1,15 @@
+package com.web_prd_demo_app_36443;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "web_prd_demo_app_36443";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "web_prd_demo_app_36443",
+  url: "https://web_prd_demo_app_36443.botics.co",
+  api: "https://web_prd_demo_app_36443.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
